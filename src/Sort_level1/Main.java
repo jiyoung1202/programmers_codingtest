386414af50060aa1f8dc54b21bf803fe0953d580
jiyoung lee <bestjy1202@nate.com>
2019-04-14 19:37:39 +0900

    why??????

--- src/Sort_level1/Main.java
+++ src/Sort_level1/Main.java
@@ -2,7 +2,7 @@ package Sort_level1;
 
 import java.util.Arrays;
 
-public class Main {
+public class Main{
     public static void main(String[] args) {
         int array [] = {1, 5, 2, 6, 3, 7, 4};
         int[][] commands = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
